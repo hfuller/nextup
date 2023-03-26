0da593ce657536bb86224dbd4b6895073c42b477
Hunter Fuller <hfuller@pixilic.com>
2023-03-26 01:38:49 -0500

    Make more date magic and make it easier to update without reloading

--- nextup.js
+++ nextup.js
@@ -1,17 +1,24 @@
+ONE_HOUR = 60 * 60 * 1000; /* ms */
+
 function fancyLog(category, color, message) {
 	console.log("%c[" + category + "]", "color:" + color, message);
 }
 function setStatusMessage(message) {
 	fancyLog("STATUS", 'blue', message);
+	if ( message === false ) {
+		message = "Current Schedule";
+	}
 	document.getElementById("status-message").innerHTML = message;
 }
 
-document.addEventListener("DOMContentLoaded", async function(){
-	setStatusMessage("Loading configuration");
-	const configResponse = await fetch('config.json');
-	config = await configResponse.json();
+function getTupleEndDate(thing) {
+	let durationSeconds = thing[0].duration.toSeconds();
+	let occurrenceEnd = thing[1].toJSDate();
+	occurrenceEnd.setSeconds(occurrenceEnd.getSeconds() + durationSeconds);
+	return occurrenceEnd;
+}
 
-	setStatusMessage("Loading calendar");
+async function fetchCalendarData() {
 
 	let calendarUrl = config.calendarUrl;
 	if ( config.calendarUseCorsProxy ) {
@@ -23,14 +30,12 @@ document.addEventListener("DOMContentLoaded", async function(){
 	let iCalData = await calendarResponse.text();
 	console.log(iCalData.split('\n')[0] + "...");
 
-	setStatusMessage("Reticulating splines");
-
 	fancyLog("CALUPD", 'green', "Parsing calendar");
-	jCalData = ICAL.parse(iCalData);
+	let jCalData = ICAL.parse(iCalData);
 
 	fancyLog("CALUPD", 'green', "Getting only events");
 	//We use only the last section(?) - hard coding for google calendar (HACK)
-	jCalRawEvents = jCalData[jCalData.length-1].filter(function(thing) {
+	let jCalRawEvents = jCalData[jCalData.length-1].filter(function(thing) {
 		return thing[0].toLowerCase() == "vevent";
 	});
 	//.map(function(thing) {
@@ -38,7 +43,7 @@ document.addEventListener("DOMContentLoaded", async function(){
 	//});
 
 	fancyLog("CALUPD", 'green', "Filtering " + jCalRawEvents.length + " events");
-	jCalEventOccurrenceTuples = [];
+	let jCalEventOccurrenceTuples = [];
 	for ( let rawEvent of jCalRawEvents ) {
 		let event = new ICAL.Event(new ICAL.Component(rawEvent));
 		let component = new ICAL.Component(rawEvent);
@@ -61,19 +66,76 @@ document.addEventListener("DOMContentLoaded", async function(){
 
 	fancyLog("CALUPD", 'green', "Filtering " + jCalEventOccurrenceTuples.length + " tuples");
 	jCalEventOccurrenceTuples = jCalEventOccurrenceTuples.filter(function(thing) {
-		let durationSeconds = thing[0].duration.toSeconds();
-		let occurrenceEnd = thing[1].toJSDate();
-		occurrenceEnd.setSeconds(occurrenceEnd.getSeconds() + durationSeconds);
-		//console.log("FILTER: ", thing[0].summary, " duration ", durationSeconds, " - ", thing[1], occurrenceEnd);
-		//debug = thing;
-		return occurrenceEnd > Date.now();
+		//return getTupleEndDate(thing) > Date.now();
+		//actually let's filter only stuff that ended at least an hour ago
+		return getTupleEndDate(thing) > new Date(Date.now() - ONE_HOUR);
 	});
 
 	fancyLog("CALUPD", 'green', "Sorting " + jCalEventOccurrenceTuples.length + " events");
 	jCalEventOccurrenceTuples = jCalEventOccurrenceTuples.sort(function(b,a){
 		return new Date(b[1].toJSDate()) - new Date(a[1].toJSDate());
 	});
-	console.log(jCalEventOccurrenceTuples);
 
+	return jCalEventOccurrenceTuples;
+}
+function renderCalendar(tuples) {
+	let ol = document.getElementsByTagName('ol')[0];
+
+	fancyLog("RENDER", 'orange', 'clearing list');
+	ol.replaceChildren([]);
+
+	fancyLog("RENDER", 'orange', 'doing stuff');
+	for ( let tuple of tuples ) {
+		let eSummary = document.createElement('h3');
+		eSummary.innerHTML = tuple[0].summary;
+
+		//date magic
+		let occurrenceBegin = tuple[1].toJSDate();
+		let occurrenceEnd = getTupleEndDate(tuple);
+		let now = Date.now();
+		let today = new Date().getDate();
+
+		let eTimes = document.createElement('h4');
+		let beginString = occurrenceBegin.toLocaleTimeString('default', config.timeOptions);
+		let endString = occurrenceEnd.toLocaleTimeString('default', config.timeOptions);
+		if ( occurrenceBegin.getDate() != today ) { beginString = occurrenceBegin.toLocaleString('default', config.dateOptions) + " " + beginString; }
+		if ( occurrenceEnd.getDate() != occurrenceBegin.getDate() ) { endString = occurrenceEnd.toLocaleString('default', config.dateOptions) + " " + endString; }
+		let completeString = (beginString + " - " + endString).replaceAll(":00", ""); //lol
+		eTimes.innerHTML = completeString;
+
+		let li = document.createElement('li');
+		li.appendChild(eSummary);
+		li.appendChild(eTimes);
+
+		if ( occurrenceEnd < now ) {
+			//this event already ended
+		}
+		if ( occurrenceBegin < now && occurrenceEnd > now ) {
+			//this event is in progress
+		}
+		if ( occurrenceBegin > now ) {
+			//this event hasn't started
+		}
+
+		ol.appendChild(li);
+	}
+
+	console.log(tuples);
+	fancyLog("RENDER", 'orange', "done rendering");
+}
+
+async function updateDisplay() {
+	jCalEventOccurrenceTuples = await fetchCalendarData();
+	renderCalendar(jCalEventOccurrenceTuples);
+	document.getElementById("last-updated").innerHTML = "as of " + new Date(Date.now()).toLocaleString('default');
+}
+
+document.addEventListener("DOMContentLoaded", async function(){
+	setStatusMessage("Loading configuration");
+	const configResponse = await fetch('config.json');
+	config = await configResponse.json();
+
+	setStatusMessage("Loading calendar");
+	await updateDisplay();
 	setStatusMessage(false);
 });
